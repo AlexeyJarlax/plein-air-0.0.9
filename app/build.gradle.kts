e508ad7ffb378aba46af0821b53cdf8fa3c11ede
PavlovAlex <al3ksei.pav@yandex.ru>
2024-08-23 10:59:15 +0300

    Меню входа, логин

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin") // Плагин Hilt для внедрения зависимостей.
+    kotlin("kapt") // Плагин для KAPT, необходим для работы аннотаций Hilt и Room.
 }
 
 android {
@@ -26,22 +28,58 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
 }
 
 dependencies {
+    // Основные библиотеки Android
+    implementation(libs.androidx.core.ktx.v1101)
+    implementation(libs.androidx.appcompat.v161)
+    implementation(libs.material.v190)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.runtime.android)
+    // Jetpack Compose
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.material)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.activity.compose)
+
+    // Навигация в Compose
+    implementation(libs.androidx.navigation.compose)
+
+    // Hilt для внедрения зависимостей
+    implementation(libs.hilt.android)
+    kapt(libs.google.hilt.compiler)
+
+    // Kotlin Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Room для работы с базой данных
+    implementation(libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
+    // Unit-тесты
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Android Instrumentation тесты
+    androidTestImplementation(libs.androidx.junit.v115)
+    androidTestImplementation(libs.androidx.espresso.core.v351)
+
+    // Hilt тестирование
+    androidTestImplementation(libs.hilt.android.testing)
+    kaptAndroidTest(libs.google.hilt.compiler)
+}
+
+kapt {
+    correctErrorTypes = true // Коррекция ошибок типа для KAPT.
 }
--- app/src/main/java/com/pavlovalexey/pleinair/di/AppModule.kt
+++ app/src/main/java/com/pavlovalexey/pleinair/di/AppModule.kt
@@ -1,10 +1,5 @@
 package com.pavlovalexey.pleinair.di
 
-import com.example.myapp.data.UserRepository
-import com.example.myapp.data.UserRepositoryImpl
-import com.example.myapp.data.local.UserDao
-import com.example.myapp.data.remote.ApiService
-import com.example.myapp.domain.GetUsersUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,18 +7,3 @@ import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(SingletonComponent::class)
-object AppModule {
-
-    @Provides
-    @Singleton
-    fun provideUserRepository(apiService: ApiService, userDao: UserDao): UserRepository {
-        return UserRepositoryImpl(userDao, apiService)
-    }
-
-    @Provides
-    @Singleton
-    fun provideGetUsersUseCase(userRepository: UserRepository): GetUsersUseCase {
-        return GetUsersUseCase(userRepository)
-    }
-}
