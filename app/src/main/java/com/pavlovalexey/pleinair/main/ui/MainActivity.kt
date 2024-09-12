3c6f4e6c54ae66c8579263118ebfd5b67060b777
Alexey_Pavlov <al3ksei.pav@yandex.ru>
2024-09-12 21:37:14 +0300

    ProfileFragment рефакторю 3 мусор

--- app/src/main/java/com/pavlovalexey/pleinair/main/ui/MainActivity.kt
+++ app/src/main/java/com/pavlovalexey/pleinair/main/ui/MainActivity.kt
@@ -11,7 +11,6 @@ package com.pavlovalexey.pleinair.main.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.Toast
@@ -19,34 +18,26 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.firebase.Firebase
-import com.google.firebase.appcheck.appCheck
-import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ServerValue
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.firestore.firestore
-import com.google.firebase.initialize
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.storage
 import com.pavlovalexey.pleinair.R
 import com.pavlovalexey.pleinair.auth.ui.AuthActivity
 import com.pavlovalexey.pleinair.databinding.ActivityMainBinding
 import com.pavlovalexey.pleinair.profile.ui.UserMapFragment
-import com.pavlovalexey.pleinair.utils.AppPreferencesKeys
+import com.pavlovalexey.pleinair.utils.firebase.LoginAndUserUtils
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -56,6 +47,8 @@ class MainActivity : AppCompatActivity(), UserMapFragment.OnLocationSelectedList
     @Inject lateinit var auth: FirebaseAuth
     @Inject lateinit var firestore: FirebaseFirestore
     @Inject lateinit var googleSignInClient: GoogleSignInClient
+    @Inject lateinit var loginAndUserUtils: LoginAndUserUtils
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var storage: FirebaseStorage
     private lateinit var mMap: GoogleMap
@@ -67,38 +60,32 @@ class MainActivity : AppCompatActivity(), UserMapFragment.OnLocationSelectedList
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        com.google.firebase.Firebase.initialize(this)
-        firestore = com.google.firebase.Firebase.firestore
-        auth = FirebaseAuth.getInstance()
-        storage = com.google.firebase.Firebase.storage
-        Firebase.appCheck.installAppCheckProviderFactory(
-            PlayIntegrityAppCheckProviderFactory.getInstance(),
-        )
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
-
+//        com.google.firebase.Firebase.initialize(this)
+//        firestore = com.google.firebase.Firebase.firestore
+//        auth = FirebaseAuth.getInstance()
+//        storage = com.google.firebase.Firebase.storage
+//        Firebase.appCheck.installAppCheckProviderFactory(
+//            PlayIntegrityAppCheckProviderFactory.getInstance(),
+//        )
+//
+//        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//            .requestIdToken(getString(R.string.default_web_client_id))
+//            .requestEmail()
+//            .build()
+//        googleSignInClient = GoogleSignIn.getClient(this, gso)
+//
         if (auth.currentUser == null) {
-            // Если пользователь не авторизован, перенаправляем его на AuthActivity
             startActivity(Intent(this, AuthActivity::class.java))
             finish()
             return
         }
-
-        // Установка профиля пользователя в Firestore
-        setUserProfile()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         hideSystemUI()
         progressBar = findViewById(R.id.loading_indicator)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationView.setupWithNavController(navController)
@@ -113,12 +100,17 @@ class MainActivity : AppCompatActivity(), UserMapFragment.OnLocationSelectedList
                 }
             }
         }
+
+        loginAndUserUtils.setupUserProfile { name ->
+            Toast.makeText(this, "Добро пожаловать, $name!", Toast.LENGTH_SHORT).show()
+        }
+
         setupOnlineStatusListener()
     }
 
     private fun setupOnlineStatusListener() {
         val database = FirebaseDatabase.getInstance()
-        val userStatusDatabaseRef = database.getReference("status/${FirebaseAuth.getInstance().currentUser?.uid}")
+        val userStatusDatabaseRef = database.getReference("status/${auth.currentUser?.uid}")
 
         val onlineStatus = mapOf(
             "state" to "online",
@@ -141,116 +133,39 @@ class MainActivity : AppCompatActivity(), UserMapFragment.OnLocationSelectedList
             }
 
             override fun onCancelled(error: DatabaseError) {
-                // Обработка ошибки
+                // Ошибка обработки
             }
         })
     }
 
-    private fun setUserProfile() {
-        val user = auth.currentUser ?: return
-        val userId = user.uid
-        val sharedPreferences = getSharedPreferences(AppPreferencesKeys.PREFS_NAME, Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.putString("userId", userId)
-        editor.apply()
-
-        val userDocRef = firestore.collection("users").document(userId)
-        val defaultAvatar = ""
-        val userProfile = hashMapOf(
-            "profileImageUrl" to defaultAvatar, // Используем пустую строку как дефолтное значение
-            "location" to hashMapOf(
-                "latitude" to defaultLocation.latitude,
-                "longitude" to defaultLocation.longitude
-            )
-        )
-
-        userDocRef.get()
-            .addOnSuccessListener { document ->
-                if (!document.exists()) {
-                    // Если документ не существует, создаем его
-                    userDocRef.set(userProfile)
-                        .addOnSuccessListener {
-                            Log.d(TAG, "User profile created with ID: $userId")
-                        }
-                        .addOnFailureListener { e ->
-                            Log.w(TAG, "Error creating user profile", e)
-                        }
-                } else {
-                    // Если документ существует, проверяем наличие координат
-                    if (!document.contains("location")) {
-                        userDocRef.update("location", userProfile["location"])
-                            .addOnSuccessListener {
-                                Log.d(TAG, "Default location set for user ID: $userId")
-                            }
-                            .addOnFailureListener { e ->
-                                Log.w(TAG, "Error setting default location", e)
-                            }
-                    } else {
-                        // Загрузка текущего местоположения из Firestore
-                        val location = document.get("location") as? Map<*, *>
-                        val latitude = location?.get("latitude") as? Double
-                        val longitude = location?.get("longitude") as? Double
-                        if (latitude != null && longitude != null) {
-                            selectedLocation = LatLng(latitude, longitude)
-                        }
-                    }
-                }
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error getting user profile", e)
-            }
-    }
-
     override fun onLocationSelected(location: LatLng) {
-        Log.d(TAG, "Location selected: $location")
         selectedLocation = location
-
-        // Обновление местоположения пользователя в Firestore
-        val user = auth.currentUser ?: return
-        val userId = user.uid
-        val userDocRef = firestore.collection("users").document(userId)
-
-        val updatedLocation = hashMapOf(
-            "latitude" to location.latitude,
-            "longitude" to location.longitude
-        )
-
-        userDocRef.update("location", updatedLocation)
-            .addOnSuccessListener {
-                Log.d(TAG, "User location updated to: $location")
+        loginAndUserUtils.updateUserLocation(location) { isSuccess ->
+            if (isSuccess) {
                 Toast.makeText(this, "Координаты обновлены!", Toast.LENGTH_SHORT).show()
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error updating user location", e)
+            } else {
                 Toast.makeText(this, "Ошибка координат!", Toast.LENGTH_SHORT).show()
             }
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
         mMap.setOnMapClickListener { latLng ->
             mMap.clear()
             mMap.addMarker(MarkerOptions().position(latLng).title("Выбрано местоположение"))
             selectedLocation = latLng
         }
-
-        // Установите начальную позицию карты на выбранное местоположение или значение по умолчанию
         val initialPosition = selectedLocation ?: defaultLocation
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(initialPosition, 15f))
     }
 
     fun logoutAndRevokeAccess() {
-        auth.signOut()
+        loginAndUserUtils.logout()
         googleSignInClient.revokeAccess().addOnCompleteListener {
-            // После выхода из аккаунта можно перенаправить пользователя на экран авторизации
             startActivity(Intent(this, AuthActivity::class.java))
             finish()
         }
-    }
-
-    fun onLogout() {
-        logoutAndRevokeAccess()
     }
 
     private fun hideSystemUI() {
--- app/src/main/java/com/pavlovalexey/pleinair/utils/firebase/LoginAndUserUtils.kt
+++ app/src/main/java/com/pavlovalexey/pleinair/utils/firebase/LoginAndUserUtils.kt
@@ -2,13 +2,26 @@ package com.pavlovalexey.pleinair.utils.firebase
 
 import android.app.Application
 import android.content.Context
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.pavlovalexey.pleinair.R
 import com.pavlovalexey.pleinair.utils.AppPreferencesKeys
+import javax.inject.Inject
 
 class LoginAndUserUtils(private val context: Context) {
 
-    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    @Inject
+    lateinit var auth: FirebaseAuth
+    @Inject
+    lateinit var firestore: FirebaseFirestore
+    @Inject
+    lateinit var googleSignInClient: GoogleSignInClient
+    @Inject
+    lateinit var loginAndUserUtils: LoginAndUserUtils
+
+    private val defaultLocation = LatLng(59.9500019, 30.3166718)
     private val sharedPreferences = context.getSharedPreferences(AppPreferencesKeys.PREFS_NAME, Context.MODE_PRIVATE)
     private val animals: List<String> by lazy { loadFile(R.raw.animals) }
     private val verbs: List<String> by lazy { loadFile(R.raw.verbs) }
@@ -36,6 +49,40 @@ class LoginAndUserUtils(private val context: Context) {
         val verb = verbs.random()
         val noun = nouns.random()
         return "$animal $verb $noun"
+    }
+
+    fun setupUserProfile(onProfileSet: (String) -> Unit) {
+        val user = auth.currentUser ?: return
+        val userId = user.uid
+        val userDocRef = firestore.collection("users").document(userId)
+
+        userDocRef.get().addOnSuccessListener { document ->
+            if (!document.exists()) {
+                val userName = generateRandomUserName()
+                val userProfile = hashMapOf(
+                    "profileImageUrl" to "",
+                    "userName" to userName,
+                    "location" to hashMapOf(
+                        "latitude" to defaultLocation.latitude,
+                        "longitude" to defaultLocation.longitude
+                    )
+                )
+                userDocRef.set(userProfile).addOnSuccessListener {
+                    saveUserNameLocally(userName)
+                    onProfileSet(userName)
+                }
+            } else {
+                val userName = document.getString("userName") ?: generateRandomUserName()
+                saveUserNameLocally(userName)
+                onProfileSet(userName)
+            }
+        }
+    }
+
+    private fun saveUserNameLocally(userName: String) {
+        val editor = sharedPreferences.edit()
+        editor.putString("userName", userName)
+        editor.apply()
     }
 
     companion object {
